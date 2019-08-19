79a7edd079b30a0d4ecb36cfa4e7e1063f36a00c
@viktoriyatkachuk <kievjobrule@gmail.com>
2019-08-19 14:07:19 +0300

    feat (lesson01): my first initial commit

---
+++ src/lesson01/FirstProgram.java
@@ -0,0 +1,9 @@
+package lesson01;
+
+import java.sql.SQLOutput;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello My World!!!!!!");
+    }
+}
