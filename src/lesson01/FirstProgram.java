5725bc39d96a1641b77588357a04e8f386e33b36
viktoriyatkachuk <kievjobrule@gmail.com>
2019-08-21 11:35:47 +0300

    feat (lesson01): my first initial commit number 2

--- src/lesson01/FirstProgram.java
+++ src/lesson01/FirstProgram.java
@@ -4,6 +4,6 @@ import java.sql.SQLOutput;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello My World!!!!!!");
+        System.out.println("Hello My World try 2");
     }
 }
